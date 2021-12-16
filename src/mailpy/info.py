ed0845be82c42491fd2718ffe33af21511d3c11c
carneirofc <claudiofcarneiro@hotmail.com>
2021-12-16 14:21:21 -0300

    Remove requirement from field "group" on the collection "entries"

--- src/mailpy/info.py
+++ src/mailpy/info.py
@@ -1,8 +1,8 @@
 __author__ = "Claudio F. Carneiro"
 __author_email__ = "claudiofcarneiro@hotmail.com"
 __source_url__ = "https://github.com/carneirofc/mailpy-monitor"
-__version__ = "423cb22"
-__date__ = "2021-12-09 15:57:08-03:00"
+__version__ = "5fa0a7f"
+__date__ = "2021-12-16 14:18:31-03:00"
 
 __all__ = [
     "__author__",
